90c22a2be72b606790e29d5dcdba5313aa8d94f4
simonreddy2001 <simon.reddy2001@gmail.com>
2021-10-06 10:16:10 +0200

    morepics

--- src/client/components/Helper/Pics.js
+++ src/client/components/Helper/Pics.js
@@ -9,7 +9,12 @@ const Pics = [
     'https://2rdnmg1qbg403gumla1v9i2h-wpengine.netdna-ssl.com/wp-content/uploads/sites/3/2021/02/fiberFoods-105493090-770x533-1-745x490.jpg',
     'https://images.chinahighlights.com/allpicture/2019/11/81223e71381b4ee99857c3c5_cut_800x500_61.jpg',
     'https://images.chinahighlights.com/allpicture/2019/11/7b3afb974d7946bbbe397f7c_894x670.webp',
-    'https://images.chinahighlights.com/allpicture/2019/11/a47d9323b7ae409ba96e628d_894x596.webp'
+    'https://images.chinahighlights.com/allpicture/2019/11/a47d9323b7ae409ba96e628d_894x596.webp',
+    'https://upload.wikimedia.org/wikipedia/commons/2/2f/Sicilian_caponata.jpg',
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/7/72/Parmigiana_di_melanzane.jpg/1599px-Parmigiana_di_melanzane.jpg',
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/CapponMagroMD.jpg/1920px-CapponMagroMD.jpg',
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/6/68/Braised_Quebec_rabbit_cacciatore_-_Giorgio%27s_Trattoria.jpg/1920px-Braised_Quebec_rabbit_cacciatore_-_Giorgio%27s_Trattoria.jpg',
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/2/26/Italian_Panzanella.jpg/1920px-Italian_Panzanella.jpg'
 ]
 
 export default Pics;
