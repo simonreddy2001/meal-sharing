9add48ac8ab188b821b46854400ed81ef7989e8f
simonreddy2001 <simon.reddy2001@gmail.com>
2021-10-07 10:29:12 +0200

    mobile view

--- src/client/components/Navbar/Navbar.js
+++ src/client/components/Navbar/Navbar.js
@@ -3,8 +3,9 @@ import logo from "./../../assets/images/logo.png"
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+
     useEffect(() => {
-        document.addEventListener('DOMContentLoaded', function () {
+        document.addEventListener('click', function () {
             let sidenav = document.querySelectorAll('.sidenav')
             M.Sidenav.init(sidenav, { edge: 'right' });
         })
@@ -17,8 +18,7 @@ const Navbar = () => {
                     <img src={logo} alt="logo" width="122" />
                     <Link to="/" className="brand-logo"> Meal-Sharing</Link>
                     <a href="#" data-target="mobile-nav" className="sidenav-trigger"><i className="material-icons">menu</i></a>
-                    <ul className="right">
-                        {/* hide-on-med-and-down */}
+                    <ul className="right hide-on-med-and-down">
                         <li><Link to="/meals">Meals</Link></li>
                         <li><Link to="/create-meal">Add Meal</Link></li>
                         <li><Link to="/about">About</Link></li>
